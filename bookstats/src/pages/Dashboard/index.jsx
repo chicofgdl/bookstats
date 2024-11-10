c9c0b410d9749be4a689055c41fe3f8c22d7146b
Francisco Gabriel <francisco_abada@hotmail.com>
2024-11-10 20:01:02 -0300

    feature(dashboard) Cria versão de consumo da API utilizando mês e ano
    - Abordagem utilizando apenas ano será aplicada

--- bookstats/src/pages/Dashboard/index.jsx
+++ bookstats/src/pages/Dashboard/index.jsx
@@ -1,12 +1,19 @@
-// src/pages/Dashboard/index.jsx
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 import BarChart from "../../components/Charts/BarChart";
 import PieChart from "../../components/Charts/PieChart";
 import LineChart from "../../components/Charts/LineChart";
-import { Select, MenuItem, FormControl, InputLabel, Box } from "@mui/material";
+import {
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    Box,
+    TextField,
+    Button,
+} from "@mui/material";
+import dayjs from "dayjs"; // Biblioteca para manipulação de datas
 
 const barData = [
     { genre: "Romance", value: 55 },
@@ -22,42 +29,166 @@ const pieData = [
     { id: "Aventura", label: "C", value: 20 },
 ];
 
-const lineData = [
-    {
-        id: "Romance",
-        data: [
-            { x: "2020", y: 350 },
-            { x: "2021", y: 100 },
-            { x: "2022", y: 150 },
-            { x: "2023", y: 200 },
-        ],
-    },
-    {
-        id: "Terror",
-        data: [
-            { x: "2020", y: 150 },
-            { x: "2021", y: 200 },
-            { x: "2022", y: 250 },
-            { x: "2023", y: 300 },
-        ],
-    },
-    {
-        id: "Aventura",
-        data: [
-            { x: "2020", y: 250 },
-            { x: "2021", y: 250 },
-            { x: "2022", y: 150 },
-            { x: "2023", y: 100 },
-        ],
-    },
-];
+// const lineData = [
+//     {
+//         id: "Romance",
+//         data: [
+//             { x: "2020", y: 350 },
+//             { x: "2021", y: 100 },
+//             { x: "2022", y: 150 },
+//             { x: "2023", y: 200 },
+//         ],
+//     },
+//     {
+//         id: "Terror",
+//         data: [
+//             { x: "2020", y: 150 },
+//             { x: "2021", y: 200 },
+//             { x: "2022", y: 250 },
+//             { x: "2023", y: 300 },
+//         ],
+//     },
+//     {
+//         id: "Aventura",
+//         data: [
+//             { x: "2020", y: 250 },
+//             { x: "2021", y: 250 },
+//             { x: "2022", y: 150 },
+//             { x: "2023", y: 100 },
+//         ],
+//     },
+// ];
 
 export default function Dashboard() {
     const [chartType, setChartType] = useState("bar"); // Estado para controlar o tipo de gráfico da análise de Distribuições por gênero
+    const [selectedGenres, setSelectedGenres] = useState([]); // Gêneros selecionados pelo usuário
+    const [startDate, setStartDate] = useState(""); // Data de início para o gráfico
+    const [endDate, setEndDate] = useState(""); // Data de fim para o gráfico
+    const [lineData, setLineData] = useState([]); // Dados do gráfico de linha
+
+    const availableGenres = [
+        "Action and Adventure",
+        "Arts",
+        "Biographies",
+        "Business",
+        "Children's",
+        "Classics",
+        "Comics",
+        "Cookbooks",
+        "Dictionaries",
+        "Education",
+        "Fiction",
+        "Health",
+        "History",
+        "Hobbies",
+        "Humor",
+        "Literature",
+        "Medical",
+        "Music",
+        "Mystery",
+        "Nonfiction",
+        "Philosophy",
+        "Poetry",
+        "Reference",
+        "Religion",
+        "Romance",
+        "Science",
+        "Self Help",
+        "Social Science",
+        "Sports",
+        "Technology",
+        "Travel",
+        "Young Adult",
+    ];
+    // Romance e Ficção Científica
+    // Suspense e Mistério
+    // Não Ficção Histórica
+    // Poesia e Romance
+    // Romance e Mistério
+    // Fantasia e Aventura
+    // Narrativa histórica
 
     // Função para atualizar o tipo de gráfico
     const handleChange = (event) => {
         setChartType(event.target.value);
+    };
+
+    const handleDateChange = (event, type) => {
+        if (type === "start") setStartDate(event.target.value);
+        else setEndDate(event.target.value);
+    };
+
+    const handleGenreChange = (event) => {
+        setSelectedGenres(event.target.value);
+    };
+
+    const fetchLineData = async () => {
+        if (startDate > endDate) {
+            console.log("Data de início maior que a data de fim");
+        }
+        if (
+            !startDate ||
+            !endDate ||
+            startDate > endDate ||
+            selectedGenres.length === 0
+        )
+            return;
+
+        try {
+            const fetchGenreData = async (genre) => {
+                const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&startIndex=0&maxResults=40&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`;
+
+                const response = await fetch(url);
+                const data = await response.json();
+
+                const monthlyAverages = {};
+
+                data.items.forEach((item) => {
+                    const { publishedDate, averageRating } = item.volumeInfo;
+                    if (publishedDate && averageRating) {
+                        const date = dayjs(publishedDate).format("MM-YYYY");
+                        if (!monthlyAverages[date]) {
+                            monthlyAverages[date] = { total: 0, count: 0 };
+                        }
+                        monthlyAverages[date].total += averageRating;
+                        monthlyAverages[date].count += 1;
+                    }
+                });
+
+                // Organizando os dados para o gráfico
+                const genreData = {
+                    id: genre,
+                    data: Object.keys(monthlyAverages)
+                        .sort()
+                        .map((date) => ({
+                            x: date,
+                            y:
+                                monthlyAverages[date].count > 0
+                                    ? (
+                                          monthlyAverages[date].total /
+                                          monthlyAverages[date].count
+                                      ).toFixed(2)
+                                    : 0,
+                        })),
+                };
+
+                return genreData;
+            };
+
+            const allGenresData = await Promise.all(
+                selectedGenres.map(async (genre) => {
+                    const genreData = await fetchGenreData(genre);
+                    return genreData;
+                })
+            );
+
+            setLineData(allGenresData);
+        } catch (error) {
+            console.error(
+                "Erro ao buscar dados para o gráfico de linha:",
+                error
+            );
+        }
     };
 
     return (
@@ -111,6 +242,44 @@ export default function Dashboard() {
                     <h1 className="text-3xl font-bold text-gray-800 mt-4 mb-8 self-center">
                         Gráfico de Linha
                     </h1>
+                    {/* Filtros de Data e Gênero */}
+                    <Box sx={{ display: "flex", gap: 2, mb: 4 }}>
+                        <TextField
+                            label="Data Início"
+                            type="month"
+                            value={startDate}
+                            onChange={(event) =>
+                                handleDateChange(event, "start")
+                            }
+                            InputLabelProps={{ shrink: true }}
+                        />
+                        <TextField
+                            label="Data Fim"
+                            type="month"
+                            value={endDate}
+                            onChange={(event) => handleDateChange(event, "end")}
+                            InputLabelProps={{ shrink: true }}
+                        />
+                        <FormControl sx={{ minWidth: 200 }}>
+                            <InputLabel>Selecione Gêneros</InputLabel>
+                            <Select
+                                multiple
+                                value={selectedGenres}
+                                onChange={handleGenreChange}
+                                renderValue={(selected) => selected.join(", ")}
+                            >
+                                {availableGenres.map((genre) => (
+                                    <MenuItem key={genre} value={genre}>
+                                        {genre}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <Button variant="contained" onClick={fetchLineData}>
+                            Atualizar Gráfico
+                        </Button>
+                    </Box>
+
                     <Box
                         sx={{
                             width: "100%",
@@ -118,7 +287,14 @@ export default function Dashboard() {
                             position: "relative",
                         }}
                     >
-                        <LineChart data={lineData} />
+                        {lineData.length > 0 ? (
+                            <LineChart data={lineData} />
+                        ) : (
+                            <div className="text-2xl">
+                                Selecione datas e gênero para visualizar o
+                                gráfico.
+                            </div>
+                        )}
                     </Box>
                 </div>
             </div>
